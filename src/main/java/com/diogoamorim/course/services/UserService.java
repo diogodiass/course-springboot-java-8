001420a84c216e00973977fa85514e610e902a6c
Diogo <diogo.amorim.dias@hotmail.com>
2020-12-22 11:45:57 -0300

    Service layer, component registration

---
+++ src/main/java/com/diogoamorim/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.diogoamorim.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diogoamorim.course.entities.User;
+import com.diogoamorim.course.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+			
+	}
+}
