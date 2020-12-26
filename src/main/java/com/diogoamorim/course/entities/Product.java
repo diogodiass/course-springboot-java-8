8072e260a523aaa9c985d1958db90cbed2e7a1cb
Diogo <diogo.amorim.dias@hotmail.com>
2020-12-26 13:22:07 -0300

    Product

--- src/main/java/com/diogoamorim/course/entities/Product.java
+++ src/main/java/com/diogoamorim/course/entities/Product.java
@@ -12,27 +12,35 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
-	
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String descripion;
+	private Double price;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category() {
+	public Product() {
 		
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String descripion, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.descripion = descripion;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
+
+
 
 	public Long getId() {
 		return id;
@@ -49,9 +57,33 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getDescripion() {
+		return descripion;
+	}
+
+	public void setDescripion(String descripion) {
+		this.descripion = descripion;
+	}
 	
-	public Set<Product> getProducts() {
-		return products;
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -70,7 +102,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -79,7 +111,7 @@ public class Category implements Serializable {
 		return true;
 	}
 
-
+	
 	
 	
 	
