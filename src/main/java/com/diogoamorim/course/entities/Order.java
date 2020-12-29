80701cd6c38754687b4f9720b5840a68de97de79
Diogo <diogo.amorim.dias@hotmail.com>
2020-12-29 13:07:19 -0300

    Subtotal & Total methods

--- src/main/java/com/diogoamorim/course/entities/Order.java
+++ src/main/java/com/diogoamorim/course/entities/Order.java
@@ -1,4 +1,4 @@
-	package com.diogoamorim.course.entities;
+package com.diogoamorim.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
@@ -30,24 +30,24 @@ public class Order implements Serializable {
 	private Long id;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 
 	public Order() {
 
 	}
-	
-	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
+
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		setOrderStatus(orderStatus);
@@ -70,7 +70,7 @@ public class Order implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-	
+
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
@@ -86,7 +86,7 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-			
+
 	public Payment getPayment() {
 		return payment;
 	}
@@ -95,10 +95,18 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
-	public Set<OrderItem> getItems(){
+	public Set<OrderItem> getItems() {
 		return items;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x: items) {
+			sum = sum + x.getSubTotal();
+		}
+		
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
@@ -107,7 +115,6 @@ public class Order implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-	
 
 	@Override
 	public boolean equals(Object obj) {
