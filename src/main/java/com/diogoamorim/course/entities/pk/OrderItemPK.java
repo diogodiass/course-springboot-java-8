cb65fb01a3b0ed80129cc9701da0d88806b136cb
Diogo <diogo.amorim.dias@hotmail.com>
2020-12-29 12:01:45 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/diogoamorim/course/entities/pk/OrderItemPK.java
+++ src/main/java/com/diogoamorim/course/entities/pk/OrderItemPK.java
@@ -12,27 +12,31 @@ import com.diogoamorim.course.entities.Product;
 @Embeddable
 public class OrderItemPK implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@ManyToOne
-	@JoinColumn(name="order_id")
+	@JoinColumn(name = "order_id")
 	private Order order;
-	
+
 	@ManyToOne
-	@JoinColumn(name="product_id")
+	@JoinColumn(name = "product_id")
 	private Product product;
-	
+
 	public Order getOrder() {
 		return order;
 	}
+
 	public void setOrder(Order order) {
 		this.order = order;
 	}
+
 	public Product getProduct() {
 		return product;
 	}
+
 	public void setProduct(Product product) {
 		this.product = product;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -41,7 +45,7 @@ public class OrderItemPK implements Serializable {
 		result = prime * result + ((product == null) ? 0 : product.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -63,11 +67,5 @@ public class OrderItemPK implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
-	
 
 }
